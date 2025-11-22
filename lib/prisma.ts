31c50ed036a2f59c10cd7884ac1f02d8a4afcf40
Angelito <angelluis.encarnacion2000@gmail.com>
2025-11-22 17:14:42 -0500

    Fix Prisma client import to use standard client instead of Edge

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,10 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
 
 declare global {
   var __prisma: PrismaClient | undefined
 }
 
 const connectionString = process.env.DATABASE_URL!
-
-const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
 
 export const prisma = global.__prisma ?? new PrismaClient()
 
