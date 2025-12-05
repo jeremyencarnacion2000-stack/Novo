2a7fb9316f0b452af5d6b1d5e55e77b3007640fc
Angelito <angelluis.encarnacion2000@gmail.com>
2025-12-04 22:17:19 -0500

    Restore GrokChatbot to test if it's the correct version

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ChatbotPanel from '@/chatbot/ChatbotPanel';
+import { GrokChatbot } from '@/components/ai/chatbot';
 import { DashboardShell } from '@/components/dashboard-shell';
 
 export default function AIPage() {
   return (
     <DashboardShell>
       <div className="-my-8 -mx-6 lg:-mx-8 h-full">
-        <ChatbotPanel isOpen={true} />
+        <GrokChatbot />
       </div>
     </DashboardShell>
   );
