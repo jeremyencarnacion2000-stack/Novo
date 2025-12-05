f0b7556528dade36f635ad0e1534598737a158ab
Angelito <angelluis.encarnacion2000@gmail.com>
2025-12-05 14:56:37 -0500

    Fix double scroll - add html/body height 100% and overflow hidden

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`font-sans antialiased`}>
+    <html lang="en" className="h-full">
+      <body className="h-full font-sans antialiased">
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
