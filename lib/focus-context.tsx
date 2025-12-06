8998e80c0dfb872179353184b125faa26143831b
Angelito <angelluis.encarnacion2000@gmail.com>
2025-12-06 17:28:44 -0500

    fix: Add missing useSession import in FocusProvider

--- lib/focus-context.tsx
+++ lib/focus-context.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { createContext, useContext, useState, useEffect, useRef } from 'react'
+import { useSession } from 'next-auth/react'
 
 interface FocusContextType {
   isFocusModeActive: boolean
